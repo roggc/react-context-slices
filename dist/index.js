a282a73a283f2d297027ac7a9446df54ae56cf03
roggc <roggc9@gmail.com>
2023-09-23 06:29:03 +0200

    redo changes to redux import

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { configureStore, createSlice as createReduxSlice, } from "@reduxjs/toolkit";
+import * as reduxToolkit from "@reduxjs/toolkit";
+const { configureStore, createSlice: createReduxSlice } = reduxToolkit;
 import { Provider as ReduxProvider, createSelectorHook, createDispatchHook, } from "react-redux";
 const __SET_INIT_PERSISTED_STATE_RN__ = "__SET_INIT_PERSISTED_STATE_RN__";
 const createReduxSliceWrapper = (name, reducers, initialState) => {
